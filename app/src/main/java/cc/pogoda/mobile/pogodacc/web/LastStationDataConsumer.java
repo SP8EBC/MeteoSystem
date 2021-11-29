a05a11988a05cd3bea549330ebabb3868a24f7bd
Mateusz Lubecki <sp8ebc@interia.pl>
2021-11-29 11:32:10 +0100

    consumers adjusted to new API design

--- app/src/main/java/cc/pogoda/mobile/pogodacc/web/LastStationDataConsumer.java
+++ app/src/main/java/cc/pogoda/mobile/pogodacc/web/LastStationDataConsumer.java
@@ -3,12 +3,13 @@ package cc.pogoda.mobile.pogodacc.web;
 import cc.pogoda.mobile.pogodacc.type.web.ListOfStationData;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface LastStationDataConsumer {
 
-    @GET("meteo_backend/lastStationData/")
-    Call<ListOfStationData> getLastDataForStation(@Query("station")String station,
+    @GET("meteo_backend/station/{stationName}/lastStationData/")
+    Call<ListOfStationData> getLastDataForStation(@Path("stationName")String station,
                                                   @Query("ascendingOrder")boolean ascendingOrder,
                                                   @Query("isLong")boolean isLong);
 }
--- app/src/main/java/cc/pogoda/mobile/pogodacc/web/TrendConsumer.java
+++ app/src/main/java/cc/pogoda/mobile/pogodacc/web/TrendConsumer.java
@@ -3,10 +3,11 @@ package cc.pogoda.mobile.pogodacc.web;
 import cc.pogoda.mobile.pogodacc.type.web.Trend;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface TrendConsumer {
 
-    @GET("meteo_backend/trend/")
-    Call<Trend> getTrendForStation(@Query("station")String station);
+    @GET("meteo_backend/station/{stationName}/trend/")
+    Call<Trend> getTrendForStation(@Path("stationName")String station);
 }
